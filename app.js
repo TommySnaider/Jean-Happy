31335c85372a2844b36a720f2fe8aa468f22e946
TommySnaider <tommysnaiderestupinanperez@gmail.com>
2025-07-12 19:09:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLE','SBEIDI', 'TE QUIERO'],
+			strings: [ 'FELIZ', 'CUMPLE','Yessi', 'TE QUIERO'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
